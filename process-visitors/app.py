e677e86515a9977d7eee182baa43b38ba087fb1f
Davies <djenny@8c85900f5970.ant.amazon.com>
2020-06-17 00:25:31 +1000

    Implemented all Rekognition code

--- process-visitors/app.py
+++ process-visitors/app.py
@@ -5,62 +5,42 @@ import urllib
 import logging
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
-
-
 #import requests
 
-print('Loading function')
-
-rekognition = boto3.client('rekognition')
-
+client = boto3.client('rekognition')
 
 # --------------- Helper Functions to call Rekognition APIs ------------------
 
 
 def search_face_in_collection(collection_id,bucket,key):
 
-    client=boto3.client('rekognition')
-
-  
     response=client.search_faces_by_image(CollectionId=collection_id,
                                 Image={'S3Object':{'Bucket':bucket,'Name':key}},
-                                FaceMatchThreshold=90,
-                                MaxFaces=2)
-    print(response)                            
+                                FaceMatchThreshold=70,
+                                MaxFaces=1)                            
     faceMatches=response['FaceMatches']
-    print ('Matching faces')
-    for match in faceMatches:
-            print ('FaceId:' + match['Face']['FaceId'])
-            print ('Similarity: ' + "{:.2f}".format(match['Similarity']) + "%")
-            print ('Match Found in' + collection_id )
+    if len(faceMatches) == 0:  # There is no match of this face in the collection
+        return 0, 0
+    else:
+        # We know there is only one face in the photo;
+        #  and just to be sure we have told search_faces_by_image to only return the best match.
+        #  So we are VERY sure that there is only one match to process
+        face_id=faceMatches[0]['Face']['FaceId']
+        return 1, face_id
 
 
 def add_faces_to_collection(bucket,key,collection_id):   
-    client=boto3.client('rekognition')
-    ExternalImageId = key.replace("/","")
 
     response=client.index_faces(CollectionId=collection_id,
                                 Image={'S3Object':{'Bucket':bucket,'Name':key}},
-                                ExternalImageId = ExternalImageId,
                                 QualityFilter="AUTO",
-                                DetectionAttributes=['ALL'])
-
-    print('Results for ' + key) 	
-    print('Faces indexed:')						
-    for faceRecord in response['FaceRecords']:
-         print('  Face ID: ' + faceRecord['Face']['FaceId'])
-         print('  Location: {}'.format(faceRecord['Face']['BoundingBox']))
-
-    print('Faces not indexed:')
-    for unindexedFace in response['UnindexedFaces']:
-        print(' Location: {}'.format(unindexedFace['FaceDetail']['BoundingBox']))
-        print(' Reasons:')
-        for reason in unindexedFace['Reasons']:
-            print('   ' + reason)
-    return len(response['FaceRecords'])
-
-
-
+                                MaxFaces=1)
+    # We know there is only one face in the photo;
+    #  and just to be sure we have told index_faces to only return one face.
+    #  So we are VERY sure that there is only one face_id to read
+    face_id=response['FaceRecords'][0]['Face']['FaceId']
+    return face_id
+    
 
 # --------------- Main handler ------------------
 
@@ -69,29 +49,54 @@ def lambda_handler(event, context):
     '''Demonstrates S3 trigger that uses
     Rekognition APIs to detect faces, labels and index faces in S3 Object.
     '''
-    print("Received event: " + json.dumps(event, indent=2))
-    #logger.info('## Event')
+    #print("Received event: " + json.dumps(event, indent=2))
     #logger.info(json.dumps(event))
 
     # Get the object from the event
     bucket = event['Records'][0]['s3']['bucket']['name']
     key = event['Records'][0]['s3']['object']['key']
-    #logger.info(bucket)
-    #logger.info(key)
     warrants_collection_id='warrants'
     victims_collection_id='victims'
     attendees_collection_id='attendees'
-    try:
-        
-        # Calls rekognition DetectFaces API to detect faces in S3 object
-        response = search_face_in_collection(attendees_collection_id,bucket,key)
+    try:   
+        ##################################################################
+        # Check if this is a new visitor or someone we've seen before
+        ##################################################################
+        response, face_id = search_face_in_collection(attendees_collection_id,bucket,key)
+        if (response == 0):
+            print("We have a new visitor, adding them to the visitors collection")
+            face_id = add_faces_to_collection(bucket,key,attendees_collection_id)
+            print("Updating DDB visitor table with new visitor with face_id ", face_id, "and S3 key ", key)
+            # Implement code here to add new record for face_id
+            # Are we directly updating the table, or are we sending the information to appsync?
+        else:
+            print("We have a visit by an known person. Incrementing counter in DDB for face_id ",face_id)
+            # Implement code here to update DDB table by incrementing record for face_id
+            # Are we directly updating the table, or are we sending the information to appsync?
 
- 
+        ##################################################################
+        # Check if this is a victim of human trafficking
+        ##################################################################
+        response, face_id = search_face_in_collection(victims_collection_id,bucket,key)
+        if (response == 0):
+            print("All good, this person is not registered as a victim of human trafficking")
+        else:
+            print("Alert!!! We have found a potential victim of human trafficking")
+            print("Send a victim alert with the face_id ", face_id, "and the S3 key ", key)
+            # Implement code here to send the information to appsync
 
-        # Print response to console.
-        print(response)
+        ##################################################################
+        # Check if there is an outstanding warrant for this person
+        ##################################################################
+        response, face_id = search_face_in_collection(warrants_collection_id,bucket,key)
+        if (response == 0):
+            print("All good, there are no outstanding warrants for this person")
+        else:
+            print("Alert!!! We have found a person of interest to the police")
+            print("Send a warrant alert with the face_id ", face_id, "and the S3 key ", key)
+            # Implement code here to send the information to appsync
 
-        return response
+        return 0
     except Exception as e:
         print(e)
         print("Error processing object {} from bucket {}. ".format(key, bucket) +
